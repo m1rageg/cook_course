fd14eada76cc3166e14f0364cf405be20617008d
m1rageg <egor.sergei.demin153@gmail.com>
2023-06-05 00:30:02 +0300

    add pagination

--- config/settings.py
+++ config/settings.py
@@ -67,7 +67,9 @@ MIDDLEWARE = [
 REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': [
         'rest_framework.renderers.JSONRenderer',
-    ]
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 100
 }
 
 ROOT_URLCONF = 'config.urls'
